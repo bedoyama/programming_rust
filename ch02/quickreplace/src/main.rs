5e6a3c6059c27614110826a9377607ec1ded96e0
Mauricio Bedoya <bedoyama@gmail.com>
2025-11-21 15:38:51 -0600

    qr finished

--- ch02/quickreplace/src/main.rs
+++ ch02/quickreplace/src/main.rs
@@ -1,4 +1,6 @@
+use regex::Regex;
 use std::env;
+use std::fs;
 use text_colorizer::*;
 
 #[derive(Debug)]
@@ -16,6 +18,40 @@ fn main() {
         "Replacing '{}' with '{}' in file '{}' and writing to '{}'",
         args.target, args.replacement, args.filename, args.output
     );
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}", "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
 }
 
 fn print_usage() {
@@ -48,4 +84,9 @@ fn parse_args() -> Arguments {
         filename: args[2].clone(),
         output: args[3].clone(),
     }
+}
+
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let re = Regex::new(target)?;
+    Ok(re.replace_all(text, replacement).to_string())
 }
